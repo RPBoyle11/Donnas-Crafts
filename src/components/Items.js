60569efee54437078548b986c888c0251a58fd68
RP Boyle <rpboyle11@yahoo.com>
2018-12-31 13:45:18 -0500

    added a - to product title

--- src/components/Items.js
+++ src/components/Items.js
@@ -17,7 +17,7 @@ class Items extends React.Component {
     return (
       <div className='item'>
         <img src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/48420174_966823120372003_6861683268698243072_n.jpg?_nc_cat=105&_nc_ht=scontent-iad3-1.xx&oh=beeae140dc07685009d3df0c0dc6d69a&oe=5C95B68F" alt=''></img>
-        <h1 className='item-name'>Bless This Home Wreath</h1>
+        <h1 className='item-name'>Bless This Home - Wreath</h1>
         <h2 className='item-price'>Price: $50.00</h2>
         <div className='item-buttons'>
           <button className='add-to-cart'>Add to Cart</button>
